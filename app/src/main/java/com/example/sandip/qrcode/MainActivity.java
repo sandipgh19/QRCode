2a1f055bcf8e46db1fcb4cc4c2e9a84a01b4dcaa
sandip <sandipgh19@gmail.com>
2016-12-18 17:20:33 +0530

    Update

--- app/src/main/java/com/example/sandip/qrcode/MainActivity.java
+++ app/src/main/java/com/example/sandip/qrcode/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button scan,gen;
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this,"Create Activity Call",Toast.LENGTH_LONG).show();
         gen= (Button) findViewById(R.id.gen);
         scan= (Button) findViewById(R.id.scan);
         gen.setOnClickListener(new View.OnClickListener() {
@@ -29,5 +31,29 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(next);
             }
         });
+    }
+    protected void onStart(){
+        Toast.makeText(MainActivity.this,"Start Activity Call",Toast.LENGTH_LONG).show();
+        super.onStart();
+    }
+    protected void onPause(){
+        Toast.makeText(MainActivity.this,"Pause Activity Call",Toast.LENGTH_LONG).show();
+        super.onPause();
+    }
+    protected void onRestart(){
+        Toast.makeText(MainActivity.this,"Restart Activity Call",Toast.LENGTH_LONG).show();
+        super.onRestart();
+    }
+    protected void onResume(){
+        Toast.makeText(MainActivity.this,"Resume Activity Call",Toast.LENGTH_LONG).show();
+        super.onResume();
+    }
+    protected void onStop(){
+        Toast.makeText(MainActivity.this,"Stop Activity Call",Toast.LENGTH_LONG).show();
+        super.onStop();
+    }
+    protected void onDestroy(){
+        Toast.makeText(MainActivity.this,"Destroy Activity Call",Toast.LENGTH_LONG).show();
+        super.onDestroy();
     }
 }
